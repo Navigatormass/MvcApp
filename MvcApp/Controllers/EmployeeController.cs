2858d895cd755b5f5e4d28a05dbb7251f1912a09
Siarhei Seviaryn <sseviaryn@agero.com>
2019-11-06 22:33:44 -0500

    Add project files.

---
+++ MvcApp/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+
+            Employee employee = new Employee()
+            {
+                City = "NewYork",
+                EmployeeId = 12,
+                Gender = "M",
+                Name = "Papastopulos"
+            };
+            return View();
+        }
+    }
+}
---
+++ MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "Belarus",
+                "Ukraine",
+                "Latvia",
+                "Lithaunia"
+            };
+
+            return View();
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
